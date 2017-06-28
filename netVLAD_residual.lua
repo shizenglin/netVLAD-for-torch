db76044821af272b399371a16d1d76127574b4a5
Tsun-Yi Yang <shamangary@hotmail.com>
2017-06-28 15:46:10 +0800

    Add files via upload

--- netVLAD_residual.lua
+++ netVLAD_residual.lua
@@ -4,18 +4,26 @@ function netVLAD_residual:__init(K,N,D,LR)
    parent.__init(self)
    
    self.LR = LR or 1e-4
-   self.anchor_c = torch.Tensor(K,D):cuda()
-   self.grad_anchor_c = torch.Tensor(K,D):zero():cuda()
-   self.temp_grad_anchor_c = torch.Tensor(K,D):zero():cuda()
+   self.anchor_c = torch.Tensor(K,D)
+   self.grad_anchor_c = torch.Tensor(K,D):zero()
+   self.temp_grad_anchor_c = torch.Tensor(K,D):zero()
 
    self.m = nn.Sequential()
    local ct = nn.ConcatTable()
    local a = nn.Sequential():add(nn.SelectTable(1)):add(nn.Replicate(K,2))
    local b = nn.Sequential():add(nn.SelectTable(2)):add(nn.Replicate(N,3)):add(nn.MulConstant(-1,true))
    self.m:add(ct:add(a):add(b)):add(nn.CAddTable())
-   self.m:cuda()
+   
    
    self:reset(K,D)
+end
+
+function netVLAD_residual:cuda(...)
+   self.anchor_c:cuda()
+   self.grad_anchor_c:cuda()
+   self.temp_grad_anchor_c:cuda()
+   self.m:cuda()
+   return self:type('torch.CudaTensor',...)
 end
 
 function netVLAD_residual:reset(K,D)
@@ -31,8 +39,8 @@ end
 
 
 function netVLAD_residual:updateOutput(input)
-   local nnre = nn.Replicate(input:size(1),1):cuda()
-   local input_anchor_c = nnre:forward(self.anchor_c)
+   
+   local input_anchor_c = torch.repeatTensor(self.anchor_c,input:size(1),1,1)
    output = self.m:forward({input,input_anchor_c})
 
    return output
@@ -41,8 +49,7 @@ end
 
 function netVLAD_residual:updateGradInput(input, gradOutput)
 
-   local nnre = nn.Replicate(input:size(1),1):cuda()
-   local input_anchor_c = nnre:forward(self.anchor_c)
+   local input_anchor_c = torch.repeatTensor(self.anchor_c,input:size(1),1,1)
    local grad_temp = self.m:backward({input,input_anchor_c}, gradOutput)
    self.gradInput = grad_temp[1]
    self.temp_grad_anchor_c = grad_temp[2]:sum(1)
